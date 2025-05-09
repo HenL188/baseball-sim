f7a69208f3e780410385117e9178144078d5fd00
HenL188 <henry@DESKTOP-5S581AH.>
2025-05-08 19:30:12 -0600

    added innings

--- main.c
+++ main.c
@@ -13,10 +13,10 @@ int main(void){
     int out = 0;
     Bases base;
     Score score = {0};
-    for (int i = 0; i < 30; i++) {
+    for (int i = 0; i < 40; i++) {
       ba[i] = 1;
     }
-    for (int i = 30; i < 100; i++){
+    for (int i = 40; i < 100; i++){
       ba[i] = 0;
     }
 
@@ -59,7 +59,187 @@ int main(void){
         else out++;
         if (out == 3) base_reset(&base);
       }
-        out++;
+      while(out < 6 ){
+        int a = rand() % 100;
+        int b = rand() % 100;
+        if (ba[a] == 1){
+          hit++;
+          switch(stats[b]){
+            case 1:
+              single(&base, &score);
+              break;
+            case 2:
+              _double(&base, &score);
+              break;
+            case 3:
+              triple(&base, &score);
+              break;
+            case 4:
+              home_run(&base, &score);
+              break;   
+          }
+        }
+        else out++;
+        if (out == 6) base_reset(&base);
+      }
+      while(out < 9 ){
+        int a = rand() % 100;
+        int b = rand() % 100;
+        if (ba[a] == 1){
+          hit++;
+          switch(stats[b]){
+            case 1:
+              single(&base, &score);
+              break;
+            case 2:
+              _double(&base, &score);
+              break;
+            case 3:
+              triple(&base, &score);
+              break;
+            case 4:
+              home_run(&base, &score);
+              break;   
+          }
+        }
+        else out++;
+        if (out == 9) base_reset(&base);
+      }
+      while(out < 12 ){
+        int a = rand() % 100;
+        int b = rand() % 100;
+        if (ba[a] == 1){
+          hit++;
+          switch(stats[b]){
+            case 1:
+              single(&base, &score);
+              break;
+            case 2:
+              _double(&base, &score);
+              break;
+            case 3:
+              triple(&base, &score);
+              break;
+            case 4:
+              home_run(&base, &score);
+              break;   
+          }
+        }
+        else out++;
+        if (out == 12) base_reset(&base);
+      }
+      while(out < 15 ){
+        int a = rand() % 100;
+        int b = rand() % 100;
+        if (ba[a] == 1){
+          hit++;
+          switch(stats[b]){
+            case 1:
+              single(&base, &score);
+              break;
+            case 2:
+              _double(&base, &score);
+              break;
+            case 3:
+              triple(&base, &score);
+              break;
+            case 4:
+              home_run(&base, &score);
+              break;   
+          }
+        }
+        else out++;
+        if (out == 15) base_reset(&base);
+      }
+      while(out < 18 ){
+        int a = rand() % 100;
+        int b = rand() % 100;
+        if (ba[a] == 1){
+          hit++;
+          switch(stats[b]){
+            case 1:
+              single(&base, &score);
+              break;
+            case 2:
+              _double(&base, &score);
+              break;
+            case 3:
+              triple(&base, &score);
+              break;
+            case 4:
+              home_run(&base, &score);
+              break;   
+          }
+        }
+        else out++;
+        if (out == 18) base_reset(&base);
+      }
+      while(out < 21 ){
+        int a = rand() % 100;
+        int b = rand() % 100;
+        if (ba[a] == 1){
+          hit++;
+          switch(stats[b]){
+            case 1:
+              single(&base, &score);
+              break;
+            case 2:
+              _double(&base, &score);
+              break;
+            case 3:
+              triple(&base, &score);
+              break;
+            case 4:
+              home_run(&base, &score);
+              break;   
+          }
+        }
+        else out++;
+        if (out == 21) base_reset(&base);
+      }
+      while(out < 24 ){
+        int a = rand() % 100;
+        int b = rand() % 100;
+        if (ba[a] == 1){
+          hit++;
+          switch(stats[b]){
+            case 1:
+              single(&base, &score);
+              break;
+            case 2:
+              _double(&base, &score);
+              break;
+            case 3:
+              triple(&base, &score);
+              break;
+            case 4:
+              home_run(&base, &score);
+              break;   
+          }
+        }
+        else out++;
+        if (out == 24) base_reset(&base);
+      }
+        int a = rand() % 100;
+        int b = rand() % 100;
+        if (ba[a] == 1){
+          hit++;
+          switch(stats[b]){
+            case 1:
+              single(&base, &score);
+              break;
+            case 2:
+              _double(&base, &score);
+              break;
+            case 3:
+              triple(&base, &score);
+              break;
+            case 4:
+              home_run(&base, &score);
+              break;   
+         }
+        }
+        else out++;
       }
 
     printf("Hit: %i\nOut: %i\n", hit, out);
